1c44bd2f7815291fdfd9363ff391e64c25727b2a
Lucas Guedes <lucasguedes778@gmail.com>
2022-07-13 09:59:26 -0300

    [MLP] get_total_cost() function.

--- MLP/src/main.cpp
+++ MLP/src/main.cpp
@@ -24,6 +24,8 @@ std::vector<Neighborhood> g_NL = {SWAP, _2_OPT, REINSERTION, OR_OPT2, OR_OPT3};
 void initialize_candidate_list(std::vector<int> &candidate_list);
 std::vector<int> construction(const double alpha);
 
+double get_total_cost(std::vector<int>s);
+
 int main(int argc, char **argv){
     std::vector<int> s;
 
@@ -31,6 +33,8 @@ int main(int argc, char **argv){
 
     s = construction(0.20);
 
+
+    std::cout << "COST: " << get_total_cost(s) << std::endl;
 
     for(auto k : s)
     {
@@ -74,4 +78,20 @@ void initialize_candidate_list(std::vector<int> &candidate_list)
     {
         candidate_list.push_back(i);
     }
+}
+
+
+double get_total_cost(std::vector<int>s)
+{
+    double total_cost = 0;
+    double latency = 0;
+    size_t seqsize = s.size();
+
+    for(size_t i = 0; i < seqsize - 1; i++)
+    {
+        latency += matrix[s[i]][s[i+1]];
+        total_cost += latency;
+    }
+
+    return total_cost;
 }
